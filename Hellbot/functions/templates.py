219da495da3b35f281d54fbd43ec99ccc2336483
PB65-Aujla <140618546+PB65-Aujla@users.noreply.github.com>
2024-02-23 06:55:25 +0400

    Update templates.py

--- Hellbot/functions/templates.py
+++ Hellbot/functions/templates.py
@@ -6,7 +6,7 @@ from Hellbot.core import ENV, db
 ALIVE_TEMPLATES = [
     (
         "•────────────────•\n"
-        "•       𝐇ᴇʟʟ𝐁ᴏᴛ 𝐈s 𝐀ʟɪᴠᴇ        •\n"
+        "•       AujlaBot 𝐈s 𝐀ʟɪᴠᴇ        •\n"
         "╭────────────────•\n"
         "╰➢ ᴏᴡɴᴇʀ » {owner}\n"
         "╰➢ ᴘʏʀᴏɢʀᴀᴍ » {pyrogram}\n"
@@ -14,7 +14,7 @@ ALIVE_TEMPLATES = [
         "╰➢ ᴘʏᴛʜᴏɴ » {python}\n"
         "╰➢ ᴜᴘᴛɪᴍᴇ » {uptime}\n"
         "╰────────────────•\n"
-        "𝖡𝗒 © @HellBot_Networks\n"
+        "𝖡𝗒 © @LIONS_OF_PUNJAB\n"
         "•────────────────•\n"
     ),
 ]
@@ -263,7 +263,7 @@ USER_INFO_TEMPLATES = [
 **➢ 𝖡𝗈𝗍:** `{isBot}`
 **➢ 𝖡𝗂𝗈:** `{bio}`
 
-**</> @HellBot_Networks**
+**</> @LIONS_OF_PUNJAB 🍀**
 """
 ]
 
@@ -281,7 +281,7 @@ CHAT_INFO_TEMPLATES = [
 **➢ 𝖡𝗈𝗍𝗌:** `{botsCount}`
 **➢ 𝖣𝖾𝗌𝖼𝗋𝗂𝗉𝗍𝗂𝗈𝗇:** `{description}`
 
-**</> @HellBot_Networks**
+**</> @LIONS_OF_PUNJAB**
 """
 ]
 
